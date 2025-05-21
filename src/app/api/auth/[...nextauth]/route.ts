12a0ce92e9e6eaa76ab9e0461062653f2ea60dc5
Luba Mathias <lucianomathiasfo@gmail.com>
2025-05-21 19:29:47 -0300

    changes3

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,4 @@
+// src/app/api/auth/[...nextauth]/route.ts
 import NextAuth from "next-auth";
 import type { OAuthConfig } from "next-auth/providers/oauth";
 
@@ -8,12 +9,11 @@ const BlingProvider = {
   clientId: process.env.BLING_CLIENT_ID!,
   clientSecret: process.env.BLING_CLIENT_SECRET!,
   authorization: {
-    url: "https://www.bling.com.br/Api/v3/oauth/authorize",
-    params: {
-      scope: "conectar",
-      redirect_uri: process.env.BLING_REDIRECT_URI, // ⬅️ importante!
-    },
+  url: "https://www.bling.com.br/Api/v3/oauth/authorize",
+  params: {
+    scope: "conectar",
   },
+},
   token: "https://www.bling.com.br/Api/v3/oauth/token",
   userinfo: "https://www.bling.com.br/Api/v3/usuarios/me",
   profile(profile: any) {
